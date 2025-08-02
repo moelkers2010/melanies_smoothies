6193cd31842c0a47962076d851c0b933c7a41f67
moelkers2010 <moelkers2010@gmail.com>
2025-08-02 15:20:34 -0500

    rearrange nutrition data on page

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True )
 
     st.write(ingredients_string)
 
@@ -50,6 +52,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, ' + name_on_order +'!', icon="✅")
     
     
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True )
+
 #st.text(smoothiefroot_response.json())
