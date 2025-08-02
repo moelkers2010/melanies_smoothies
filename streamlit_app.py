5d57f12de5bf04ab9a9da1a3fa729880a7530de1
moelkers2010 <moelkers@pm.me>
2025-08-02 15:06:17 -0500

    Update streamlit_app.py
    adds smoothiefroot api call and requests library import

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 cnx = st.connection("snowflake")
 session = cnx.session()
@@ -43,8 +44,11 @@ if ingredients_list:
 
     time_to_insert = st.button('Submit Order')
 
+
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order +'!', icon="✅")
     
     
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruits/watermellon")
+st.text(smoothiefroot_response)
